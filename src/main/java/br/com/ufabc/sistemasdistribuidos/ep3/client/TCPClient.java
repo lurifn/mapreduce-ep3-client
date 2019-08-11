f3c1c0cae08fbc50dc82ce6c927292e0ff74ea64
Luana Nascimento <lluana13@hotmail.com>
2019-08-10 23:07:03 -0300

    identificando client

--- src/main/java/br/com/ufabc/sistemasdistribuidos/ep3/client/TCPClient.java
+++ src/main/java/br/com/ufabc/sistemasdistribuidos/ep3/client/TCPClient.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Scanner;
 
 class TCPClient {
@@ -11,30 +13,50 @@ class TCPClient {
 	private Scanner scanner;
 
 	private TCPClient(InetAddress serverAddress, int serverPort) throws Exception {
-        this.socket = new Socket(serverAddress, serverPort);
-        this.scanner = new Scanner(System.in);
-    }
+		this.socket = new Socket(serverAddress, serverPort);
+		this.scanner = new Scanner(System.in);
+	}
+	
+	private void stop() throws IOException {
+		socket.close();
+	}
 
-	private void start() throws IOException {
-		String input;
-		while (true) {
-			input = scanner.nextLine();
-			PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
-			out.println(input);
-			out.flush();
-		}
+	private void start() throws Exception {
+		// lê o json com a lista de urls
+		String urls = readFileAsString("lista/lista.txt");
+		System.out.println("Enviando urls: "+urls);
+		
+		//envia as urls pro coordenador
+		PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+		out.println(urls);
+		out.flush();
+	}
+
+	public static String readFileAsString(String fileName) throws Exception {
+		String data = "";
+		data = new String(Files.readAllBytes(Paths.get(fileName)));
+		return data;
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.out.println("Digite o host:");
-		String host = new Scanner(System.in).nextLine();
+		System.out.println("CLIENT");
+		String host;
 		
-		System.out.println("Digite a porta:");
-		int porta = Integer.parseInt(new Scanner(System.in).nextLine());
+		//pode receber o host do coordenador por argumento ou não
+		if(args.length > 0)
+			host = args[0];
+		else
+			host = "localhost";
 		
-		TCPClient client = new TCPClient(InetAddress.getByName(host), porta);
+		// abre conexao com o coordenador
+		TCPClient client = new TCPClient(InetAddress.getLocalHost(), 8080);
 
 		System.out.println("\r\nConnected to Server: " + client.socket.getInetAddress());
+
+		// faz a solicitação
 		client.start();
+		
+		//fecha o socket
+		client.stop();
 	}
 }
